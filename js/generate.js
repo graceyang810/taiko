491689bffef111c0e197286d7c61083ef101308e
graceyang810 <grace@sadness2.local>
2014-04-19 10:28:46 +0800

    希望没弄错。。

---
+++ js/generate.js
@@ -0,0 +1,101 @@
+var m_name1 = ["慕容", "任", "于", "厉", "钟离", "唐", "东方", "敖", "公孙", "南宫", "邵",  "司徒",  "尉迟", "司空", "尹", "明", "西门",  "归海", "莫", "尚", "上官"];
+// var m_name1 = ["赵","钱","孙","李","周","吴","郑","王","冯","陈","褚","卫","蒋","沈","韩","杨","朱","秦","尤","许","何"];
+var m_name2 = ["爵", "逸", "寒", "封", "萧", "云", "燚", "轩", "海", "元", "天", "寂" ];
+// var m_name2 = ["长", "栓", "大", "来", "狗", "守", "傻", "福", "屎", "二", "胖", "臭"];
+var m_name3 = ["言", "洛", "鸿", "涯", "夜", "痕", "清", "飞", "尘", "阳", "武", "遥", "风", "空", "遐", "竹", "涵", "偌", "御", "伦", "滨", "浪", "璘", "勇", "恒", "基", "明", "岚", "舜", "翰", "遐"];
+// var m_name3 = ["娟", "妮", "腿", "娣", "球", "坑", "年", "岁", "娃", "毛", "剩", "姑", "英", "妹", "肥", "霞", "狗", "虎", "花", "凤", "定", "村", "蛋", "妞", "木", "翠", "爱", "财", "石", "美", "发"];
+
+var f_name1 = ["慕容", "任", "于", "厉", "钟离", "唐", "东方", "敖", "白", "南宫", "竺", "司徒", "尉迟", "司空", "蓝", "邵", "西门", "颜", "上官", "欧阳", "尚", "莫" ];
+var f_name2 = ["真", "碧", "凝", "竹", "若", "雨", "紫", "影", "亦", "伊", "允", "冰"];
+var f_name3 = ["菲", "星", "芮", "悠", "馨", "香", "爱", "翎", "洛", "轩", "儿", "萱", "雪", "月", "嫣", "凌", "珣", "痕", "荫", "茹", "忆", "婷", "舞", "琦", "汐", "嫌", "钰", "心", "韵", "然", "嫣"];
+
+ 	
+$(document).ready(function(){
+	$( ".sex" ).change(function() {
+ 		if($(".sex:checked").val() == "male"){
+ 			// console.log("male");
+ 			for(n = 1; n <= 5; n++){
+ 				$("#"+"name_slide"+n).empty();
+ 				strname = "<p style='margin-top:20px;margin-left:30%;color:white;font-size:60px;'>"+generatemalename();+"</p>";
+ 				$("#name_slide" + n).html(strname); 
+ 			}
+ 			for(m = 1; m <= 10; m++){
+ 				$("#"+"avator_slider"+m).empty();
+ 				stravator = "<img style='margin-top:45px;margin-left:40%;color:white;font-size:60px;width:20%;' src='../images/m"+m+".png'>";
+				$("#avator_slider" + m).html(stravator);
+ 			}
+		}
+		if($(".sex:checked").val() == "female"){
+			// console.log("female");
+
+ 			for(n = 1; n <= 5; n++){
+ 				$("#"+"name_slide"+n).empty();
+ 				strname = "<p style='margin-top:20px;margin-left:30%;color:white;font-size:60px;'>"+generatefemalename();+"</p>";
+ 				$("#name_slide" + n).html(strname); 
+ 			}
+ 			for(m = 1; m <= 10; m++){
+ 				$("#"+"avator_slider"+m).empty();
+ 				stravator = "<img style='margin-top:45px;margin-left:40%;color:white;font-size:60px;width:20%;' src='../images/f"+m+".png'>";
+				$("#avator_slider" + m).html(stravator);
+ 			}
+			
+		}
+ 	});
+
+	$( "#submit" ).click(function(){
+		if(code == con_code){
+		$('#submit').hide();
+		$('#submitOK').show();
+
+		var id = getRandom(9999);
+		strID = id;
+		$("#id").html(strID);	
+
+		$("#name").html($(".active > p").html());
+		$("#avator").attr("src",$(".active > img").attr("src"));	
+		}
+		else{
+			alert("两次密码输入不一致，请重新设置！");
+		}
+	});
+})
+
+	function getRandom(n){
+		return Math.floor(Math.random()*n+1);
+	}
+	function changename(){
+		if($(".sex:checked").val() == "female"){
+			for(n = 1; n <= 5; n++){
+				$("#"+"name_slide"+n).empty();
+				strname = "<p style='margin-top:20px;margin-left:30%;color:white;font-size:60px;'>"+generatefemalename();+"</p>";
+			document.getElementById("name_slide"+n).innerHTML += strname; 
+			}
+		}
+
+		if($(".sex:checked").val() == "male"){
+			for(n = 1; n <= 5; n++){
+				$("#"+"name_slide"+n).empty();
+				strname = "<p style='margin-top:20px;margin-left:30%;color:white;font-size:60px;'>"+generatemalename();+"</p>";
+			document.getElementById("name_slide"+n).innerHTML += strname; 
+			}
+		}
+	}	
+	
+	function generatemalename(){
+		var m_ran1 = getRandom(20);
+		var m_ran2 = getRandom(11);
+		var m_ran3 = getRandom(30);
+		var m_name = m_name1[m_ran1] + m_name2[m_ran2] + m_name3[m_ran3];
+		// console.log(m_name);
+		return m_name;
+	}
+	function generatefemalename(){
+		var f_ran1 = getRandom(21);
+		var f_ran2 = getRandom(11);
+		var f_ran3 = getRandom(30);
+		var f_name = f_name1[f_ran1] + f_name2[f_ran2] + f_name3[f_ran3];
+		// console.log(f_name);
+		return f_name;
+	}
+
+	
---
+++ js/loadsong.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	$.getJSON("../json/get_rhthem.json",function(data){
+		var $song = $("#song"); 
+		var $vssong = $("#vssong"); 
+		var $rhythm = $("#rhythm");
+		var strrhythm = "";
+		var strHtml = ""; 
+		var vsstrHtml = ""; 
+		$song.empty(); 
+		$rhythm.empty();
+		$vssong.empty(); 
+
+		strHtml = "<label>对战曲目："+data.song_name+"</label>";	
+		$song.html(strHtml);
+
+		vsstrHtml = "<label style='color:white;top:10%;left:20%;position:absolute;z-index:6;font-size:25px;'>对战曲目："+data.song_name+"</label>";				
+		$vssong.html(vsstrHtml);
+
+		var insertText =  "<audio id='gamesong' src='"+ data.url+"'></audio>"; 
+		document.getElementById("song").innerHTML = document.getElementById("song").innerHTML+insertText;
+		document.getElementById("gamesong").addEventListener("ended",function() {
+			miss = data.rhythm.length - perfect -cool;
+	        document.location.href = "./result.html?perfect="+perfect+"&cool="+cool+"&miss="+miss+"&combo="+combo;
+	    });
+	    
+
+		for(var i in data.rhythm){
+			strrhythm = "<img class='rhythm' style='width:71px;top:46px;z-index:4;'  id='rhythm"+ i +"' src='../images/"+ data.rhythm[i].color +".png'>";
+			document.getElementById("rhythm").innerHTML = document.getElementById("rhythm").innerHTML+strrhythm; 
+		}		
+		
+		//节奏点自适应屏幕缩放
+		for(k in data.rhythm){			
+			var times = document.body.clientWidth/1377*0.83;
+			var wid = parseInt(document.getElementById("rhythm" + k).style.width)*times;
+			$("#" +"rhythm" + k).css("width",wid);
+
+			var change = document.body.clientWidth/1377*0.83*165/2;
+			var newtop = parseInt(document.getElementById("rhythm" + k).style.top) + change - wid/2;
+			$("#" +"rhythm" + k).css("top",newtop);
+		}
+	}); 	
+})
